0541ebc2b7144bf3deda005183a19dfe67d11420
Willians Faria <dev.willians.faria@gmail.com>
2022-01-18 09:06:10 -0300

    ensure hash returns the correct hashed string

--- __mocks__/argon2.ts
+++ __mocks__/argon2.ts
@@ -2,8 +2,12 @@ import argon2 from 'argon2'
 
 export default {
 	isValid: true,
+	hashedString: 'hashed_string',
 	async verify (hash: string, plain: string | Buffer, options?: argon2.Options | undefined) {
 		if (hash === plain) return this.isValid
 		throw new Error()
+	},
+	async hash (plainString: string) {
+		return this.hashedString
 	}
 }
--- src/infra/argon-crypto.spec.ts
+++ src/infra/argon-crypto.spec.ts
@@ -18,7 +18,11 @@ class ArgonCrypto implements Crypto {
 	}
 
 	async hash (rawString: string): Promise<string> {
-		return ''
+		try {
+			return await argon2.hash(rawString)
+		} catch (_) {
+			throw new CryptoError('There was an error hashing the provided string')
+		}
 	}
 }
 
@@ -36,6 +40,7 @@ describe('ArgonCrypto', () => {
 		const isValid = await sut.compare(rawString, hashedString)
 
 		expect(isValid).toBe(true)
+		expect(isValid).toBe((argon2 as any).isValid)
 	})
 
 	it('Should throw an error if hash don\'t match', async () => {
@@ -45,5 +50,15 @@ describe('ArgonCrypto', () => {
 
 		expect(sut.compare(rawString, hashedString)).rejects.toThrow()
 		expect(sut.compare(rawString, hashedString)).rejects.toEqual(new CryptoError('Plain string doesn\'t match hashed string'))
+	})
+
+	it('Should hash a raw string if argon hash completes', async () => {
+		const { sut } = makeSut()
+		const rawString = 'any_string'
+
+		const hashedString = await sut.hash(rawString)
+
+		expect(hashedString).toBe('hashed_string')
+		expect(hashedString).toBe((argon2 as any).hashedString)
 	})
 })
